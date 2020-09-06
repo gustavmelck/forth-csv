8734c0b33c06e0a4ad521fb880892fc602f8b924
Gustav Melck <g@gu.pe>
2020-09-06 11:25:06 +0200

    completed basic csv reader

--- forth-csv.fs
+++ forth-csv.fs
@@ -1,76 +1,97 @@
 \ forth csv parser by gustav melck, sep 2020
 \ vim: fdm=marker
 
-marker reset
-
 private{  \ {{{
 
 : gthrow  ( ior addr u -- )  2 pick  if  type >r ." ; forth-csv error " r@ . cr r> throw  else  2drop drop  then  ;
 
-create buffer 128 chars allot           0 value bufferlen
+0 value ubuf            0 value ubuflen         0 value max-ubuflen
 
-: buffer+  ( addr u -- )  dup >r buffer bufferlen + swap cmove  bufferlen r> + to bufferlen  ;
-: 0buffer+  ( addr u -- )  0 to bufferlen  buffer+  ;
+: ubuf-available  ( -- u )  max-ubuflen ubuflen -  ;
+
+: ubuf+  ( addr u -- )  ubuf-available min dup >r ubuf ubuflen + swap cmove  ubuflen r> + to ubuflen  ;
+: 0ubuf+  ( addr u -- )  0 to ubuflen  ubuf+  ;
 
 0 value fid
 
-create c 1 chars allot
+create c 0 c,               0 value prev-c-eol?
 
-: next-c  ( -- #chars )  c 1 fid read-file s" next-c error1" gthrow  ;
+0 value next-csv-field#     -1 value (last-csv-field#)
+
+: next-csv-field#0  ( -- )  0 to next-csv-field#  ;
+: next-csv-field#+1  ( -- )  next-csv-field# 1+ to next-csv-field#  ;
+
+: next-c  ( -- #chars )  c c@ dup 10 = swap 13 = or to prev-c-eol?  c 1 fid read-file s" next-c error1" gthrow  ;
 
 defer in-quoted-field
 
 : at-quoted-field-end?  ( -- ok? )
     r> drop  next-c 1 <>  if  false  else
         c c@ case
-            [char] "  of  c 1 buffer+  in-quoted-field  endof
-            true swap
+            [char] ,  of  next-csv-field#+1 true  endof
+            10        of  next-csv-field#0 true  endof
+            13        of  next-csv-field#0 true  endof
+            drop c 1 ubuf+  true in-quoted-field  \ "true" is to trigger a second r> drop
         endcase
     then  ;
 
-: (in-quoted-field)  ( -- ok? )
-    r> drop  next-c 1 <>  if  false  else
+: (in-quoted-field)  ( double-r-drop? -- ok? )  \ if the deferred word is called a double r> drop is required
+    if  r> drop  then  r> drop  next-c 1 <>  if  false  else
         c c@ case
             [char] "  of  at-quoted-field-end?  endof
-            drop c 1 buffer+ recurse
+            drop c 1 ubuf+ false recurse
         endcase
     then  ;
-
 ' (in-quoted-field) is in-quoted-field
 
+: -ubuf-trailing  ( -- )  ubuf ubuflen -trailing to ubuflen drop  ;
+
 : in-bare-field  ( -- ok? )
-    r> drop  next-c 1 <>  if  false  else
+    r> drop  next-c 1 <>  if  -ubuf-trailing false  else
         c c@ case
-            [char] ,  of  true  endof
-            10        of  true  endof
-            13        of  true  endof
-            drop c 1 buffer+  recurse
+            [char] ,  of  -ubuf-trailing next-csv-field#+1 true  endof
+            10        of  -ubuf-trailing next-csv-field#0 true  endof
+            13        of  -ubuf-trailing next-csv-field#0 true  endof
+            drop c 1 ubuf+  recurse
         endcase
     then  ;
 
-: before-field  ( -- ok? )
+: (before-field)  ( -- ok? )
     r> drop  next-c 1 <>  if  false  else
         c c@ case
-            [char] "  of  in-quoted-field  endof
-            [char] ,  of  true  endof
-            10        of  true  endof
-            13        of  true  endof
-            drop c 1 buffer+  in-bare-field
+            [char] "  of  true in-quoted-field  endof
+            [char] ,  of  next-csv-field#+1 true  endof
+            10        of  prev-c-eol? 0=  if  next-csv-field#0 true  else  recurse  then  endof
+            13        of  prev-c-eol? 0=  if  next-csv-field#0 true  else  recurse  then  endof
+            bl        of  recurse  endof
+            drop c 1 ubuf+  in-bare-field
         endcase
     then  ;
+: before-field  ( -- ok? )  (before-field)  ;
 
 }private  \ }}}
 
 : with-csv-file-id  ( fid -- )  to fid  ;
-: close-csv-file  ( -- )  fid close-file s" close-csv-file error1" gthrow  ;
 
-: csv-field>buffer  ( -- ok? )  0 to bufferlen  before-field  ;
+: csv-field>buffer  ( addr u -- u' ok? )
+    to max-ubuflen to ubuf  0 to ubuflen  next-csv-field# to (last-csv-field#)  before-field  ubuflen swap  ;
 
-: type-buffer  ( -- )  buffer bufferlen type  ;
+: last-csv-field#  ( -- u )  (last-csv-field#)  ;
 
 privatize
 
+\ tests {{{
+create buffer 128 chars allot
+
+: type-buffer  ( u -- )  buffer swap type  ;
+
 : (test)  ( in-loop? -- )
-    if  r> drop  then  field>buffer  if  ." next:" type-buffer cr  true recurse  then  ;
+    if  r> drop  then
+    buffer 128 csv-field>buffer 0=  if  drop  else
+        last-csv-field# . ." :" type-buffer ." ;" cr  true recurse
+    then  ;
 : test  ( -- )  s" test.csv" r/o open-file throw with-csv-file-id  cr false (test)  close-csv-file  ;
+
+test .s
+\ }}}
 
